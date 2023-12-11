a1b27699301f131f20b9bef5410b8a18c1675ee7
crivera53 <147225431+crivera53@users.noreply.github.com>
2023-12-10 19:22:25 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+// src/App.js
+
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addTask = () => {
+    if (inputValue.trim() !== '') {
+      setTasks([...tasks, { text: inputValue, completed: false }]);
+      setInputValue('');
+    }
+  };
+
+  const completeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = true;
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter task..."
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li
+            key={index}
+            style={{ textDecoration: task.completed ? 'line-through' : 'none' }}
+            onClick={() => completeTask(index)}
+          >
+            {task.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
